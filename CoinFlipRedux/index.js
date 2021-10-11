4caac0d263a60d2a7ef11fd4eb5dfe762258a047
SuhasSDSU <spindiproli4798@sdsu.edu>
2021-10-10 19:08:23 -0700

    Changes in JS
    Changes made in coinFlipRedux & OddEven

--- CoinFlipRedux/index.js
+++ CoinFlipRedux/index.js
@@ -1,12 +1,10 @@
 var coinFlip;
 
 for(var i = 0 ; i < 10 ; i++){
-    coinFlip = Math.random();
-}
-coinFlip =  Math.round(coinFlip);
-console.log(coinFlip)
-if(coinFlip === 0 ){
-    console.log('Heads')
-} else {
-    console.log('Tails');
+    coinFlip =  Math.round(Math.random(coinFlip));    
+    if(coinFlip === 0 ){
+        console.log('Heads')
+    } else {
+        console.log('Tails');
+    }
 }
--- OddOrEven/index.js
+++ OddOrEven/index.js
@@ -1,7 +1,7 @@
 for( var i = 0; i <= 15 ; i++){
     if(i %2 === 0){
-        document.writeln(i," is even<br>");
+        console.log(i," is even");
     } else {
-        document.writeln(i," is odd<br>");
+        console.log(i," is odd");
     }
 }
